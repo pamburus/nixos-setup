b5b92e56e7de564f349c988470a80d10fb607fee
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-12-18 00:48:23 +0100

    feat: added powerlevel10k configuration

--- configuration.nix
+++ configuration.nix
@@ -5,235 +5,223 @@
 { config, pkgs, lib, ... }:
 
 let
-	# Fetch the Flake
-	flake = builtins.getFlake "github:pamburus/hl";
+  # Fetch the Flake
+  flake = builtins.getFlake "github:pamburus/hl";
 
-	# Extract the 'hl' package for your system
-	hl = flake.packages.${pkgs.system}.default;
+  # Extract the 'hl' package for your system
+  hl = flake.packages.${pkgs.system}.default;
 in
 {
-	imports =
-		[ # Include the results of the hardware scan.
-			./hardware-configuration.nix
-			<home-manager/nixos>
-		];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
 
-	# Package manager settings
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Package manager settings
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-	# Bootloader.
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-	networking.hostName = "nixos"; # Define your hostname.
-	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-	# Configure network proxy if necessary
-	# networking.proxy.default = "http://user:password@proxy:port/";
-	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-	# Enable networking
-	networking.networkmanager.enable = true;
+  # Enable networking
+  networking.networkmanager.enable = true;
 
-	# Set your time zone.
-	time.timeZone = "Europe/Belgrade";
+  # Set your time zone.
+  time.timeZone = "Europe/Belgrade";
 
-	# Select internationalisation properties.
-	i18n.defaultLocale = "en_US.UTF-8";
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
 
-	# Enable the X11 windowing system.
-	services.xserver.enable = true;
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
-	# Enable the GNOME Desktop Environment.
-	services.xserver.displayManager.gdm.enable = true;
-	services.xserver.desktopManager.gnome.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
-	# Configure keymap in X11
-	services.xserver.xkb = {
-		layout = "us";
-		variant = "mac";
-	};
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "mac";
+  };
 
-	# Set the cursor theme and size
-	environment.variables = {
-		XCURSOR_THEME = "GoogleDot-Black";
-		XCURSOR_SIZE = "24";
-	};
+  # Set the cursor theme and size
+  environment.variables = {
+    XCURSOR_THEME = "GoogleDot-Black";
+    XCURSOR_SIZE = "24";
+  };
 
-	# Enable CUPS to print documents.
-	services.printing.enable = false;
+  # Enable CUPS to print documents.
+  services.printing.enable = false;
 
-	# Enable sound with pipewire.
-	hardware.pulseaudio.enable = false;
-	security.rtkit.enable = true;
-	services.pipewire = {
-		enable = true;
-		alsa.enable = true;
-		alsa.support32Bit = true;
-		pulse.enable = true;
-		# If you want to use JACK applications, uncomment this
-		#jack.enable = true;
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
 
-		# use the example session manager (no others are packaged yet so this is enabled by default,
-		# no need to redefine it in your config for now)
-		#media-session.enable = true;
-	};
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 
-	# Enable touchpad support (enabled default in most desktopManager).
-	# services.xserver.libinput.enable = true;
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
-	# Disable some problematic systemd servicess
-	systemd.services.prlshprint.wantedBy = lib.mkForce [];
+  # Disable some problematic systemd servicess
+  systemd.services.prlshprint.wantedBy = lib.mkForce [];
 
-	# List packages installed in system profile. To search, run:
-	# $ nix search wget
-	environment.systemPackages = with pkgs; [
-		alacritty
-		alacritty-theme
-		atool
-		bat
-		fd
-		git
-		gnomeExtensions.toggle-alacritty
-		go
-		google-cursor
-		hl
-		home-manager
-		httpie
-		lsd
-		micro
-		nerdfonts
-		ripgrep
-		vscodium
-		wget
-		xclip
-		zsh
-		zsh-powerlevel10k
-	];
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    alacritty
+    alacritty-theme
+    atool
+    bat
+    fd
+    git
+    gnomeExtensions.toggle-alacritty
+    go
+    google-cursor
+    hl
+    home-manager
+    httpie
+    lsd
+    micro
+    nerdfonts
+    ripgrep
+    vscodium
+    wget
+    xclip
+    zsh
+    zsh-powerlevel10k
+  ];
 
-	# Configure default settings for all users
-	users.defaultUserShell = pkgs.zsh;
+  # Configure default settings for all users
+  users.defaultUserShell = pkgs.zsh;
 
-	# Define a user account. Don't forget to set a password with ‘passwd’.
-	users.users.pamburus = {
-		isNormalUser = true;
-		description = "pamburus";
-		extraGroups = [ "networkmanager" "wheel" ];
-		shell = pkgs.zsh;
-		packages = with pkgs; [];
-	};
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.pamburus = {
+    isNormalUser = true;
+    description = "pamburus";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [];
+  };
 
-	# Install firefox.
-	programs.firefox.enable = true;
+  # Install firefox.
+  programs.firefox.enable = true;
 
-	# Install zsh
-	programs.zsh = {
-		enable = true;
-		enableCompletion = true;
-		syntaxHighlighting.enable = true;
-		shellAliases = {
-			ll = "lsd -lah";
-			update = "sudo nixos-rebuild switch";
-			pbcopy = "xclip -selection clipboard";
-			pbpaste = "xclip -selection clipboard -o";
-		};
-		ohMyZsh = {
-			enable = true;
-			plugins = [ "git" ];
-			theme = "robbyrussell";
-		};
-	};
+  # Install zsh
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      ll = "lsd -lah";
+      update = "sudo nixos-rebuild switch";
+      pbcopy = "xclip -selection clipboard";
+      pbpaste = "xclip -selection clipboard -o";
+    };
+  };
 
-	# Configure hl
-	environment.etc."hl/config.json".text = builtins.toJSON {
-		theme = "tc24d-b2";
-	};
+  # Configure hl
+  environment.etc."hl/config.json".text = builtins.toJSON {
+    theme = "tc24d-b2";
+  };
 
-	# Configure micro
-	environment.etc."micro/settings.json".text = builtins.toJSON {
-		hltaberrors = true;
-		hltrailingws = true;
-		rmtrailingws = true;
-	};
+  # Configure micro
+  environment.etc."micro/settings.json".text = builtins.toJSON {
+    hltaberrors = true;
+    hltrailingws = true;
+    rmtrailingws = true;
+  };
 
-	# Install micro plug-ins
-	environment.etc."micro/plug/detectindent" = {
-		source = pkgs.fetchFromGitHub {
-			owner = "dmaluka";
-			repo = "micro-detectindent";
-			rev = "v1.1.0";
-			sha256 = "sha256-5bKEkOnhz0pyBR2UNw5vvYiTtpd96fBPTYW9jnETvq4=";
-		};
-	};
+  # Install micro plug-ins
+  environment.etc."micro/plug/detectindent" = {
+    source = pkgs.fetchFromGitHub {
+      owner = "dmaluka";
+      repo = "micro-detectindent";
+      rev = "v1.1.0";
+      sha256 = "sha256-5bKEkOnhz0pyBR2UNw5vvYiTtpd96fBPTYW9jnETvq4=";
+    };
+  };
 
-	# Enable the gnome-keyring secrets vault.
-	# Will be exposed through DBus to programs willing to store secrets.
-	services.gnome.gnome-keyring.enable = true;
+  # Enable the gnome-keyring secrets vault.
+  # Will be exposed through DBus to programs willing to store secrets.
+  services.gnome.gnome-keyring.enable = true;
 
-	security.polkit.enable = true;
+  security.polkit.enable = true;
 
-	# Some programs need SUID wrappers, can be configured further or are
-	# started in user sessions.
-	# programs.mtr.enable = true;
-	# programs.gnupg.agent = {
-	#   enable = true;
-	#   enableSSHSupport = true;
-	# };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-	# List services that you want to enable:
+  # List services that you want to enable:
 
-	# Enable the OpenSSH daemon.
-	# services.openssh.enable = true;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
-	# Open ports in the firewall.
-	# networking.firewall.allowedTCPPorts = [ ... ];
-	# networking.firewall.allowedUDPPorts = [ ... ];
-	# Or disable the firewall altogether.
-	# networking.firewall.enable = false;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-	# This value determines the NixOS release from which the default
-	# settings for stateful data, like file locations and database versions
-	# on your system were taken. It‘s perfectly fine and recommended to leave
-	# this value at the release version of the first install of this system.
-	# Before changing this value read the documentation for this option
-	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "24.11"; # Did you read the comment?
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
 
-	home-manager.users.root = { lib, pkgs, ... }: {
-		# The state version is required and should stay at the version you
-		# originally installed.
-		home.stateVersion = "24.11";
+  home-manager.users.root = { lib, pkgs, ... }: {
+    # The state version is required and should stay at the version you
+    # originally installed.
+    home.stateVersion = "24.11";
 
-		imports = [ ./home-manager-common.nix ];
-	};
+    imports = [ ./home-manager-common.nix ];
 
-	home-manager.users.pamburus = { lib, pkgs, ... }: {
-		# The state version is required and should stay at the version you
-		# originally installed.
-		home.stateVersion = "24.11";
+    home.file.".p10k.zsh" = {
+      source = "/etc/nixos/dotfiles/root/.p10k.zsh";
+    };
+  };
 
-		imports = [ ./home-manager-common.nix ];
+  home-manager.users.pamburus = { lib, pkgs, ... }: {
+    # The state version is required and should stay at the version you
+    # originally installed.
+    home.stateVersion = "24.11";
 
-		# Configure VS Code
-		programs.vscode = {
-			enable = true;
-			package = pkgs.vscodium;
-			extensions = with pkgs.vscode-extensions; [
-				dracula-theme.theme-dracula
-				golang.go
-				yzhang.markdown-all-in-one
-			];
-		};
+    imports = [ ./home-manager-common.nix ];
 
-		programs.zsh = {
-			enable = true;
-			plugins = [
-				{
-					name = "powerlevel10k";
-					src = pkgs.zsh-powerlevel10k;
-					file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-				}
-			];
-		};
-	};
+    # Configure VS Code
+    programs.vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+      extensions = with pkgs.vscode-extensions; [
+        dracula-theme.theme-dracula
+        golang.go
+        yzhang.markdown-all-in-one
+      ];
+    };
+  };
 }
--- home-manager-common.nix
+++ home-manager-common.nix
@@ -1,76 +1,100 @@
 { config, lib, pkgs, ... }:
 {
-	dconf.settings = {
-		"org/gnome/desktop/peripherals/keyboard" = {
-			delay = lib.hm.gvariant.mkUint32 250;
-			repeat-interval = lib.hm.gvariant.mkUint32 25;
-			repeat = true;
-		};
-	};
+  dconf.settings = {
+    "org/gnome/desktop/peripherals/keyboard" = {
+      delay = lib.hm.gvariant.mkUint32 250;
+      repeat-interval = lib.hm.gvariant.mkUint32 25;
+      repeat = true;
+    };
+  };
 
-	# Configure zsh
-	programs.zsh = {
-		enable = true;
-		autosuggestion.enable = true;
-	};
+  # Configure zsh
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+    initExtraFirst =
+      ''
+        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+        # Initialization code that may require console input (password prompts, [y/n]
+        # confirmations, etc.) must go above this block; everything else may go below.
+        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+        fi
+      '';
+    initExtra =
+      ''
+        # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+      '';
+  };
+  home.file.".p10k.zsh" = {
+    source = lib.mkDefault "/etc/nixos/dotfiles/user/.p10k.zsh";
+  };
 
-	# Configure git
-	programs.git = {
-		enable = true;
-		extraConfig = {
-			push = { autoSetupRemote = true; };
-			init = { defaultBranch = "main"; };
-			safe = { directory = "/etc/nixos"; };
-		};
-	};
+  # Configure git
+  programs.git = {
+    enable = true;
+    extraConfig = {
+      push = { autoSetupRemote = true; };
+      init = { defaultBranch = "main"; };
+      safe = { directory = "/etc/nixos"; };
+    };
+  };
 
-	# Configure Alacritty
-	programs.alacritty = {
-		enable = true;
-		settings = {
-			mouse = { hide_when_typing = false; };
-			cursor = {
-				style = {
-					shape = "Beam";
-					blinking = "On";
-				};
-			};
-			colors = {
-				cursor = {
-					text = "CellBackground";
-					cursor = "#d17277";
-				};
-			};
-			font = {
-				normal = {
-					family = "LiterationMono Nerd Font Mono";
-					style = "Regular";
-				};
-				bold = {
-					style = "Bold";
-				};
-				italic = {
-					style = "Italic";
-				};
-				size = 10;
-			};
-		};
-	};
+  # Configure Alacritty
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      mouse = { hide_when_typing = false; };
+      cursor = {
+        style = {
+          shape = "Beam";
+          blinking = "On";
+        };
+      };
+      colors = {
+        cursor = {
+          text = "CellBackground";
+          cursor = "#d17277";
+        };
+      };
+      font = {
+        normal = {
+          family = "LiterationMono Nerd Font Mono";
+          style = "Regular";
+        };
+        bold = {
+          style = "Bold";
+        };
+        italic = {
+          style = "Italic";
+        };
+        size = 10;
+      };
+    };
+  };
 
-	# Configure micro
-	home.file.".config/micro/settings.json".text = builtins.toJSON {
-		hltaberrors = true;
-		hltrailingws = true;
-		rmtrailingws = true;
-		tabsize = 4;
-		"*.nix" = {
-			tabsize = 2;
-		};
-	};
-	home.file.".config/micro/plug" = {
-		source = "/etc/micro/plug";
-	};
+  # Configure micro
+  home.file.".config/micro/settings.json".text = builtins.toJSON {
+    hltaberrors = true;
+    hltrailingws = true;
+    rmtrailingws = true;
+    tabsize = 4;
+    "*.nix" = {
+      tabsize = 2;
+    };
+  };
+  home.file.".config/micro/plug" = {
+    source = "/etc/micro/plug";
+  };
 
-	# Let Home Manager install and manage itself.
-	programs.home-manager.enable = true;
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
