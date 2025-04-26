d99f333137f20a9bfc6f1d3691ddd7061647bd42
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2025-04-26 13:21:05 +0200

    build: update home manager zsh config

--- home/zsh.nix
+++ home/zsh.nix
@@ -11,23 +11,23 @@
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
     ];
-    initExtraFirst =
-      ''
+    initContent = lib.mkMerge [ 
+      (lib.mkOrder 100 ''
         # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
         # Initialization code that may require console input (password prompts, [y/n]
         # confirmations, etc.) must go above this block; everything else may go below.
         if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
           source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
         fi
-      '';
-    initExtra =
-      ''
+      '')
+      (lib.mkOrder 1000 ''
         # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
         [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
         # Configure atuin
         ! command -V atuin >/dev/null || eval "''$(atuin init zsh --disable-up-arrow)"
-      '';
+      '')
+      ];
   };
 
   # Configure p10k
