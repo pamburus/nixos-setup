8cf95e97638018c32679244ae61f79fd6d7fb675
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2025-07-09 23:45:13 +0200

    chore: update gnome configuration

--- system/gnome.nix
+++ system/gnome.nix
@@ -14,8 +14,8 @@
   ];
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Enable the gnome-keyring secrets vault.
   # Will be exposed through DBus to programs willing to store secrets.
